ec96937cd6e849be3e241c62babcf97355de4fb2
wbanks <william.banks@cds-snc.ca>
2024-03-14 15:53:14 -0400

    Added prompt page, index empty awaiting notify2050 changes

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Bev's Government Helper!</title>
-        <link rel="icon" hr ef="/favicon.ico" />
-      </Head>
-
-      <Header
-        left={<h1>Bev</h1>}
-        right={<p>Windy 11°</p>}
-      />
-
-      <main>
-        <h1>How can I help you?</h1>
-        <input type="text" />
-        <Link href="/p1eng">Go</Link>
-      </main >
-
-      <Footer />
-    </div >
-  )
+    return (
+        <p>notify2050 goes here ^_^</p>
+    )
 }
