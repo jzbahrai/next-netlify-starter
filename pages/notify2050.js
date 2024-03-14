1255b7c20987bfc9daa98512b324d7d243b120a2
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2024-03-14 15:32:06 -0400

    fix

--- pages/notify2050.js
+++ pages/notify2050.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -18,6 +19,9 @@ export default function Notify2050() {
             <h2>Notify in 2050, is a Government adaptive AI tool that is the most reliable method of delivery to public recipients</h2>
           </header>
           <p>Notify becomes a layer in the many gov APIs. The APIs are accessible by everyone, from public to companies offering services.</p>
+          <Link href="/p1eng">
+            <a>Next</a>
+          </Link>
         </section>
       </main>
 
