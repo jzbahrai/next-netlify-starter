55ca4bd8fd3c2c9a19a7abab1d564e2b1720ce9a
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2024-03-14 15:14:08 -0400

    fix

---
+++ pages/p1spanish.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function p1eng() {
+  // Define your activity data in Spanish
+  const recentActivity = [
+    "Solicita la Seguridad de Vejez",
+    "Visitar a un amigo en Estados Unidos",
+    "Puse mi casa en venta"
+  ];
+
+  const currentActivity = [
+    "Vender la casa",
+    "Me mudo de Canadá",
+    "Mudanza a Estados Unidos",
+    "Mi fiesta de jubilación",
+    "Cosas que quiero probar",
+    "Posibles trabajos que quiero probar",
+    "Mantener el contacto con los colegas"
+  ];
+
+  const futureActivity = [
+    "Solicitar un visado de trabajo en Estados Unidos",
+    "Cambiar de dirección"
+  ];
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Página de estado personal de Bev</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Bev" />
+        
+        <section>
+          <h1>Actividad reciente</h1>
+          <ul>
+            {recentActivity.map((activity, index) => (
+              <li key={index}>{activity}</li>
+            ))}
+          </ul>
+        </section>
+
+        <section>
+          <h1>Actividad actual</h1>
+          <ul>
+            {currentActivity.map((activity, index) => (
+              <li key={index}>{activity}</li>
+            ))}
+          </ul>
+        </section>
+
+        <section>
+          <h1>En el futuro</h1>
+          <ul>
+            {futureActivity.map((activity, index) => (
+              <li key={index}>{activity}</li>
+            ))}
+          </ul>
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
