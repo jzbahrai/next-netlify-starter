0c43d8988e4d48faf347b17f3b9d51c2fd2a5d06
wbanks <william.banks@cds-snc.ca>
2024-03-14 15:36:41 -0400

    Add input and link to p1eng, fix top bar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,16 +10,16 @@ export default function Home() {
         <title>Bev's Government Helper!</title>
         <link rel="icon" hr ef="/favicon.ico" />
       </Head>
-
-      <main>
+      <div class='topbar'>
         <Header
           left={<h1>Bev</h1>}
           right={<p>Weather goes here</p>}
         />
-        <div class='container'>
-          <h1>How can I help you?</h1>
-          <input type="text" />
-        </div>
+      </div>
+      <main>
+        <h1>How can I help you?</h1>
+        <input type="text" />
+        <Link href="/p1eng">Go</Link>
       </main >
 
       <Footer />
